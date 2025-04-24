25ce6b3ec5678c3bad5e5235618f2652d7a98634
Waru779 <marobence1@gmail.com>
2025-04-24 17:09:24 +0200

    gyak10

--- php/includes/menu.php
+++ php/includes/menu.php
@@ -9,13 +9,14 @@ require_once 'config.php';
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <?phpforeach ($menu as $item) { ?>
-            # code...
-        }
+        <?php foreach ($menu as $item) { ?>
+    
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" href="<?=$item['link']?>">
+            <?= $item['text']?>
+          </a>
         </li>
-        <?php} ?>
+        <?php } ?>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
         </li>
